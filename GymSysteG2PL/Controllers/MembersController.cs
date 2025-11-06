2e628be7b860e1950f223cebf59cb7ab42e789a5
Ethar7 <nabihethar@yahoo.com>
2025-11-06 10:21:49 +0200

    Get Member Details

--- GymSysteG2PL/Controllers/MembersController.cs
+++ GymSysteG2PL/Controllers/MembersController.cs
@@ -12,10 +12,27 @@ namespace GymSysteG2PL.Controllers
             _memberService = memberService;
         }
 
+        #region Get All Members
         public IActionResult Index()
         {
             var members = _memberService.GetAllMembers();
             return View(members);
         }
+        #endregion
+    
+        #region Get Member Details
+        public ActionResult MemberDetails(int id)
+        {
+            if (id <= 0)
+                return RedirectToAction(nameof(Index));
+
+            var MemberDetails = _memberService.GetMemberDetails(id);
+
+            if (MemberDetails == null)
+                return RedirectToAction(nameof(Index));
+            return View(MemberDetails);
+        }
+
+        #endregion
     }
 }
